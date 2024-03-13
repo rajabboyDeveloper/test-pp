61811a27195a7b4fd73dfa75b6e2ddb1b415bbc3
rajabboy <coder@rajabboys-MacBook-Pro.local>
2024-03-13 17:53:19 +0500

    ya doasidnasdas

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,34 @@ function App() {
   }, []);
 
   return (
-    <div className="father">
-      {val.map((items, index) => {
-        return (
-          <div key={index} className="card">
-            <img src={items.url} alt="" />
-            <h2>{items.title}</h2>
-            <p>{items.content}</p>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className="father">
+        {val.map((items, index) => {
+          return (
+            <div key={index} className="card">
+              <img src={items.url} alt="" />
+              <h2>{items.title}</h2>
+              <p>{items.content}</p>
+            </div>
+          );
+        })}
+      </div>
+      <h1>main</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+        delectus reprehenderit aut sunt vero temporibus a ex. Culpa excepturi
+        earum neque voluptatum natus accusantium! At voluptatem asperiores
+        nostrum illo pariatur placeat earum nemo iste laboriosam consequuntur
+        tempore accusamus voluptas expedita accusantium nobis possimus cumque
+        necessitatibus obcaecati molestiae molestias, sequi dolore. Doloremque,
+        doloribus? Nihil aliquam reprehenderit, velit beatae aut consectetur
+        mollitia id ipsum quos minus porro doloremque aperiam possimus commodi
+        laboriosam soluta ea consequuntur quam quidem blanditiis necessitatibus
+        modi eum veniam? Nobis modi veniam totam. Quia dolore esse tempora quos
+        sunt, reiciendis nesciunt quidem voluptate obcaecati minima ab libero
+        nemo repudiandae.
+      </p>
+    </>
   );
 }
 
